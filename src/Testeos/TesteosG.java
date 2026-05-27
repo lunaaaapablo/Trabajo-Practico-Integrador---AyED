3b2317e5583cff7cfe2ecea147a4aa323799ad84
WMasi-404 <maximolopez0808@gmail.com>
2026-05-27 16:10:33 -0300

    primer testeo

--- src/Testeos/TesteosG.java
+++ src/Testeos/TesteosG.java
@@ -1,13 +1,18 @@
 package Testeos;
-import grafo.contenedores.*;
-import grafo.dirigido.*;
-import grafo.recursos.*;
-import archivos.*;
+import grafo.GrafoMaterias;
+import archivos.Lectura;
 
 
 public class TesteosG {
 
     public static void main(String[] args) {
+        Lectura lec = new Lectura();
+        GrafoMaterias t1 = lec.cargarMaterias("src/Archivos txt/MateriasdeTUP.txt");
+        for(int i = 0; i < t1.getCapacidad(); i++){
+            System.out.println(t1.getMateria(i).getId() + " " + t1.getMateria(i).getNombre() + " " + t1.getMateria(i).getAnio());
+        }
         
+        lec.cargarCorrelativas("src/Archivos txt/CorrelativasTUP.txt", t1);
+        t1.getGrafo().muestraGrafo();
     }
 }
--- src/grafo/GrafoMaterias.java
+++ src/grafo/GrafoMaterias.java
@@ -1,3 +1,4 @@
+
 package grafo;
 
 import grafo.contenedores.ListaDoubleLinkedL;
